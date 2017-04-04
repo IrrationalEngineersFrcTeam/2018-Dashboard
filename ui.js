788ac537ecb123c9f4ccab18a96e1325784c3fce
Matthew <matt.orie@gmail.com>
2017-04-04 13:03:51 -0400

    Bunch issues fixed, completed timer code.

--- ui.js
+++ ui.js
@@ -1,4 +1,7 @@
- 
+
+/* to start the Dashboard:  py -3.6 -m pynetworktables2js --robot [IP address]   */
+
+
 var ui = {
 
     robotConnection: document.getElementById('robotConnection'),
@@ -6,6 +9,7 @@ var ui = {
     control: document.getElementById('controlSelect'),
     autoMonitor: document.getElementById('autoMonitor'),
     timer: document.getElementById('timer'),
+    timeWarning: document.getElementById('timeWarning'),
 
 autoSelect: {
 
@@ -13,39 +17,40 @@ autoSelect: {
     button1: document.getElementById('button1'),
     button2: document.getElementById('button2'),
     button3: document.getElementById('button3'),
-        buttonVal1: false,
-        buttonVal2: false,
-        buttonVal3: false,
+
+
 },
 
 
-DriveMonitor: {
-
-     autoL: false,
-     autoR: false,
-     autoM: false,
-},
 
     Dial: {
         container: document.getElementById('rotationDisplay'),
         indicator: document.getElementById('rotationDial'),
         Value: 0,
         visualValue: 0,
-        resetValue: 0,
-    },
+        resetValue: 0
+    }
 
-}
+};
 
 
-NetworkTables.addRobotConnectionListener(onRobotConnection, true);
 NetworkTables.addGlobalListener(onValueChanged, true);
+NetworkTables.addRobotConnectionListener(onRobotConnection, true);
+
 
 
 
 function onRobotConnection(connected) {
-    var state = connected ? '[ Robot connected ]' : '[ Robot disconnected ]';
+    var state = connected ?   'Robot connected' : 'Robot disconnected'   ;
     console.log(state);
     ui.robotConnection.innerHTML = state;
+}
+
+
+
+
+
+
 
     function onValueChanged(key, value) {
         // Sometimes, NetworkTables will pass booleans as strings. This corrects for that.
@@ -55,6 +60,8 @@ function onRobotConnection(connected) {
 
         switch (key) {
 
+
+
             case '/SmartDashboard/driveSelect':
                 ui.drive.value = value;
                 break;
@@ -63,31 +70,33 @@ function onRobotConnection(connected) {
                 ui.control.value = value;
                 break;
 
-            case '/SmartDashboard/autoSelect':
+
+
+            case '/SmartDashboard/autoSelect/input1':
                 ui.button1.onclick = function () {
-                    ui.buttonVal1 = true;
-                }
+                       ui.input1 = true;
+                };
                 break;
 
-            case '/SmartDashboard/autoSelect':
+            case '/SmartDashboard/autoSelect/input2':
                 ui.button2.onclick = function () {
-                    ui.buttonVal2 = true;
-                }
+                    ui.input2 = true;
+                };
                 break;
 
-            case '/SmartDashboard/autoSelect':
+            case '/SmartDashboard/autoSelect/input3':
                 ui.button3.onclick = function () {
-                    ui.buttonVal3 = true;
-                }
+                    ui.input3 = true;
+                };
                 break;
 
-            case '/SmartDashboard/autoMonitor/DriveSubsystem | autoL | autoR | autoM' :
+            case '/SmartDashboard/autoL' :
 
-                if (ui.autoL = true) {
+                if (ui.autoL === true) {
                     ui.autoMonitor.innerHTML = '[Left autonomous program running]';
-                } else if (ui.autoR = true) {
+                } else if (ui.autoR === true) {
                     ui.autoMonitor.innerHTML = '[Right autonomous program running]';
-                } else if (ui.autoM = true) {
+                } else if (ui.autoM === true) {
                     ui.autoMonitor.innerHTML = '[Center autonomous program running]';
                 } else {
                     ui.autoMonitor.innerHTML = '[No autonomous program running]';
@@ -95,18 +104,22 @@ function onRobotConnection(connected) {
 
                 break;
 
-            case '/SmartDashboard /DriveSubsystem/ NavX | Yaw':
-                ui.Dial.Value = value;
+            case '/SmartDashboard/Yaw':
+
+                ui.Dial.Value = value ;
                 ui.Dial.visualValue = Math.floor(ui.Dial.Value - ui.Dial.resetValue);
                 if (ui.Dial.visualValue < 0) { // Corrects for negative values
-                    ui.Dial.visualValue += 360;
+                    ui.Dial.visualValue += 180;
                 }
                 ui.Dial.indicator.style.transform = ('rotate(' + ui.Dial.visualValue + 'deg)');
 
             case '/SmartDashboard/timeRunning':
 
+
+
                 var s = 135;
-                if (value < 0) {
+
+                if (value === true) {
 
                     ui.timer.style.color = "#00d500";
 
@@ -114,21 +127,25 @@ function onRobotConnection(connected) {
                         s--; // Subtracts one second
 
                         var m = Math.floor(s / 60);
-                        // Create seconds number that will actually be displayed after minutes are subtracted
+
                         var visualS = (s % 60);
 
-                        // Add leading zero if seconds is one digit long, for proper time formatting.
+
                         visualS = visualS < 10 ? '0' + visualS : visualS;
 
                         if (s < 0) {
                             // Stop countdown when timer reaches zero
                             clearTimeout(countdown);
                             return;
-                        } else if (s <= 15) {
-                            // Flash timer if less than 15 seconds left
+                        } else if (s <= 30) {
+
                             ui.timer.style.color = 'red';
-                        } else if (s <= 60) {
-                            // Solid red timer when less than 30 seconds left.
+
+                            ui.timeWarning.innerHTML = "Go for the Climb!"
+                            ui.timeWarning.style.color = (s % 2 === 0) ? 'red' : 'transparent';
+
+                        } else if (s <= 75) {
+
                             ui.timer.style.color = 'yellow';
                         }
                         ui.timer.innerHTML = m + ':' + visualS;
@@ -137,13 +154,15 @@ function onRobotConnection(connected) {
                     s = 135;
                 }
 
+
                 NetworkTables.setValue(key, false);
+
                 break;
-
-
         }
 
-    };
+
+
+    }
 
 
     ui.Dial.container.onclick = function () {
@@ -151,9 +170,6 @@ function onRobotConnection(connected) {
         ui.Dial.offset = ui.Dial.Value;
 
         onValueChanged('/SmartDashboard/drive/navX/yaw', ui.Dial.Value);
-    }
-
-}
-
+    };
 
 
